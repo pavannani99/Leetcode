5dc4430c7f1b23d1e4ebe2ce065d29600ae3cc52
simhadri pavan kumar <167475789+pavannani99@users.noreply.github.com>
2025-01-21 20:16:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.py
@@ -1,12 +1,6 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        # n=len(gain)
-        # self[n]={0}
-        # self[0]=gain[0]
-        #     for i in range(n):
-        #         self[i]=self[i-1]-gain[i]
-        #         gain.sort()
-        # return gain[n-1]
+        
         a=0
         b=0
         for g in gain:
@@ -14,3 +8,11 @@ class Solution:
             b=max(b,a)
         return b
         
+
+        dry run
+        -5,1,5,0,-7
+        g=-5   a=g  max(0,-5)==0
+        g=-5+1  a=-4  max(0,-4)==0
+        g=-4+5  a=1    max(0,1)==1
+        g=0 max(1,1)==1
+        # g=1-7  a=-6 max(1,-6)==1
