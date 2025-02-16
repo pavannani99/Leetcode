2ff2b1451696c783cc35ea4b33233906a49ac8d6
simhadri pavan kumar <167475789+pavannani99@users.noreply.github.com>
2025-02-17 01:37:58 +0530

    Time: 3 ms (8.3%), Space: 18 MB (25.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,7 +7,24 @@ class Solution:
         for i in range(n):
             nums1[index]=nums2[i]
             index=index+1
-        return nums1.sort()
+        for j in range(len(nums1) - 1):
+            for k in range(j + 1, len(nums1)):
+                if nums1[j] > nums1[k]:
+                    nums1[j], nums1[k] = nums1[k], nums1[j]  # Swap elements
+        # middle=0
+        # start=0
+        # end=len(nums1)-1
+        # while(start>end):
+        #     if start>middle:
+        #         nums1.swap(start,middle)
+        #         middle+=1
+        #     else if(end<middle):
+        #         nums1.swap(middle,end)
+        # else:
+        #     return nums1    
+            
+            
+
             
             
         
