1b0ecfaf4f4d02c084cdbdf9d9b06115eec558f7
simhadri pavan kumar <167475789+pavannani99@users.noreply.github.com>
2025-02-17 01:59:14 +0530

    Time: 0 ms (100%), Space: 17.7 MB (71.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,15 +3,31 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        index=m
-        for i in range(n):
-            nums1[index]=nums2[i]
-            index=index+1
-        for j in range(len(nums1) - 1):
-            for k in range(j + 1, len(nums1)):
-                if nums1[j] > nums1[k]:
-                    nums1[j], nums1[k] = nums1[k], nums1[j]  # Swap elements
-        # middle=0
+        p1,p2,p=m-1,n-1,m+n-1
+
+        while p1>=0 and p2>=0:
+            if nums1[p1]>nums2[p2]:
+                nums1[p]=nums1[p1]
+                p1-=1
+
+            else:
+                nums1[p]=nums2[p2]
+                p2-=1
+            p-=1
+          # If any elements are left in nums2, copy them (nums1 elements are already in place)
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
+        # index=m
+        # for i in range(n):
+        #     nums1[index]=nums2[i]
+        #     index=index+1
+        # for j in range(len(nums1) - 1):
+        #     for k in range(j + 1, len(nums1)):
+        #         if nums1[j] > nums1[k]:
+        #             nums1[j], nums1[k] = nums1[k], nums1[j]  # Swap elements
+        # # middle=0
         # start=0
         # end=len(nums1)-1
         # while(start>end):
