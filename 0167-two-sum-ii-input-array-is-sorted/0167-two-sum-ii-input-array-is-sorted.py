1f948d3e77443444d5cd40f212d0e1d9ef2f8f4d
simhadri pavan kumar <167475789+pavannani99@users.noreply.github.com>
2025-06-03 08:18:42 +0530

    Time: 3529 ms (5.03%), Space: 18.6 MB (33.2%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,11 +1,7 @@
 class Solution:
-    def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        for i in range(len(numbers)):
-            tres = target - numbers[i]  # Find the required complement
-            
-            # Check for complement in the list after the current index to avoid using the same number twice
-            if tres in numbers[i + 1:]:
-                j = numbers.index(tres, i + 1)  # Find the index of the complement starting from i+1
-                return [i + 1, j + 1]  # Return 1-based indices
-
-        return []  # If no valid pair is found
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+       for i in range(len(numbers)):
+            diff = target - numbers[i]
+            if diff in numbers[i + 1:]:
+                j = numbers.index(diff, i + 1)
+                return [i+1, j+1]
