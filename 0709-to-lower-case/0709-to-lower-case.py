b79477026c07ff85a74a2f15885f36e4419a3772
simhadri pavan kumar <167475789+pavannani99@users.noreply.github.com>
2025-10-04 02:35:05 +0530

    Time: 0 ms (100%), Space: 12.5 MB (52.73%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s = s.strip().lower()
+        return s   # remove spaces + lowercase
+
