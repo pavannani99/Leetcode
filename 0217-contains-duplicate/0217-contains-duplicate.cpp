01114c94a70a62f39d1bd4359fc73f04bed65b15
simhadri pavan kumar <167475789+pavannani99@users.noreply.github.com>
2025-10-29 01:49:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-           if(mp.count(nums[i])>=1){
-            return true;
-           }
-           mp[nums[i]]=i;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]]++;
+            if (mp.count(nums[i]) > 0)
+                return true;
+                mp[nums[i]] = 1; 
         }
-           return false;
+        return false;
     }
 };
