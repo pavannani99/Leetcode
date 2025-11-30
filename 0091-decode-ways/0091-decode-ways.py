8bfe422d9bcbb8a27dfde8a8cbe720159eed78ed
simhadri pavan kumar <167475789+pavannani99@users.noreply.github.com>
2025-11-30 11:38:25 +0530

    Time: 4 ms (25.98%), Space: 12.8 MB (7.43%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,26 +1,21 @@
-class Solution:
-    def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0':  # If string starts with '0', return 0
-            return 0
+class Solution(object):
+    def numDecodings(self, s):
+        dp = {len(s): 1}
 
-        alpha = "abcdefghijklmnopqrstuvwxyz"
-        alpha_arr = [alpha[i] for i in range(26)]  # Storing 'a' to 'z' for reference (not used)
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+
+            if s[i] == "0":
+                return 0
+
+            res = dfs(i + 1)
+
+            if (i + 1 < len(s) and 
+                (s[i] == "1" or (s[i] == "2" and s[i+1] in "0123456"))):
+                res += dfs(i + 2)
+
+            dp[i] = res
+            return res
         
-        set1 = []  # Used to store decoding possibilities (renamed from `set`)
-        n = len(s)
-        
-        for i in range(n):
-            set1.append(s[i])  # Store each character separately
-        
-        count = [0] * (n + 1)  # This will store decoding counts
-        count[0], count[1] = 1, 1  # Base case: empty string and single digit
-        
-        for j in range(1, n):
-            if '1' <= s[j] <= '9':  # Single-digit valid case
-                count[j + 1] += count[j]
-            
-            two_digit = int(s[j - 1:j + 1])  # Two-digit number check
-            if 10 <= two_digit <= 26:
-                count[j + 1] += count[j - 1]
-        
-        return count[n]
+        return dfs(0)
